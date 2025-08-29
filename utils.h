efc7cb9189193109352565a19c894a9e4f4c7fab
sergefriendly <se-altair@mail.ru>
2025-08-29 13:47:35 +0900

    Create utils.h

---
+++ utils.h
@@ -0,0 +1,12 @@
+namespace utils {
+
+// Эта функция сразу говорит о качестве
+void replaceEscapeSequencesIn(char *const code);
+
+// Показывает работу со строками без STL
+bool areStringsEqual(const char *a, const char *b);
+
+// Демонстрирует алгоритмическое мышление
+bool isSubsequence(const char *sequence, const char *subsequence);
+
+} // namespace utils
